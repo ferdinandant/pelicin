f30c821d923f11b272f304017fa0eb67d39ee58a
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2020-06-29 03:07:13 +0700

    Simplify chapter toc css

--- packages/global/layout/ChapterSidebar.tsx
+++ packages/global/layout/ChapterSidebar.tsx
@@ -3,22 +3,19 @@ export function ChapterSidebar() {
 
   return (
     <>
-      <aside>{sidebarContent}</aside>
+      <aside>
+        <div id="chapter-sidebar-container">{sidebarContent}</div>
+      </aside>
 
       <style jsx>{`
         aside {
-          position: fixed;
-          top: var(--header-height);
-          height: calc(100vh - var(--header-height));
-          width: var(--sidebar-width);
-          box-sizing: border-box;
           flex: 0 0 var(--sidebar-width);
-          padding: 16px 24px;
           background-color: #f5f5f5;
         }
-        a {
-          text-decoration: none;
-          color: grey;
+
+        #chapter-sidebar-container {
+          position: sticky;
+          top: var(--header-height);
         }
       `}</style>
     </>
