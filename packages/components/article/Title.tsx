a60829e2ddd3269866e6e8c5c3079b8143b38956
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2021-01-03 22:15:17 +0700

    More cleanup

--- packages/components/article/Title.tsx
+++ packages/components/article/Title.tsx
@@ -1,9 +1,16 @@
 import React, { ReactNode } from 'react';
-import style from './Title.css';
+
+// ================================================================================
+// TYPES/CONST
+// ================================================================================
 
 type Props = {
   children: ReactNode;
 };
+
+// ================================================================================
+// MAIN
+// ================================================================================
 
 export default function Title(props: Props) {
   const { children } = props;
@@ -14,7 +21,24 @@ export default function Title(props: Props) {
         <h1>{children}</h1>
       </div>
 
-      <style jsx>{style}</style>
+      <style jsx>{`
+        div {
+          margin-bottom: 2.5rem;
+        }
+
+        h1 {
+          font-size: 2.5rem;
+          font-weight: bold;
+          border: initial;
+          border-bottom: 1px solid #e5e5e5;
+          margin-left: -16px;
+          margin-right: -16px;
+          padding-left: 16px;
+          padding-right: 16px;
+          padding-bottom: 16px;
+          line-height: 3.2rem;
+        }
+      `}</style>
     </>
   );
 }
