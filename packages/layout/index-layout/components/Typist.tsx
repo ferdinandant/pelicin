c2f7b801c6756a33900f06dcabdbb6c9fa1b2301
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2021-01-11 02:56:20 +0700

    Make content not jump

--- packages/layout/index-layout/components/Typist.tsx
+++ packages/layout/index-layout/components/Typist.tsx
@@ -81,13 +81,16 @@ export default function Typist() {
       <style jsx>{`
         div {
           display: inline-block;
-          padding: var(--spacing-xs) var(--spacing-s);
+          padding: var(--spacing-xs) var(--spacing-xs);
           border-bottom: 1px solid var(--color-gray-3);
         }
         span {
           display: inline-block;
-          height: calc(var(--font-size-title-2) + 4px);
           font-size: var(--font-size-title-2);
+        }
+        span::before,
+        span::after {
+          content: '${'\u00a0'}';
         }
       `}</style>
     </>
