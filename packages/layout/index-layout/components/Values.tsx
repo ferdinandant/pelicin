6f6481a5fea58c883331059a31576459a9f5cf9a
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2021-01-12 02:21:32 +0700

    Sweeten values box

--- packages/layout/index-layout/components/Values.tsx
+++ packages/layout/index-layout/components/Values.tsx
@@ -46,9 +46,13 @@ export default function Values() {
       </div>
 
       <style jsx>{`
+        /* https://www.heropatterns.com/ */
         .root {
-          background-color: var(--color-gray-1);
           padding: var(--spacing-m);
+          background-color: #e4e7eb;
+          background-image: url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-6 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%23cbd2d9' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
+          border-top: 4px solid var(--color-gray-2);
+          border-bottom: 4px solid var(--color-gray-2);
         }
         .content {
           max-width: 800px;
@@ -65,7 +69,7 @@ export default function Values() {
         .titleRow h2 {
           font-weight: 600;
           font-size: var(--font-size-title-2);
-          color: var(--color-gray-9);
+          color: var(--color-gray-8);
         }
         .titleRow h2::before {
           content: '— ';
