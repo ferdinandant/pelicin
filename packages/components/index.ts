d8831f9ac87fab9ae7f55e495896f8f42fd702f3
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2021-01-16 17:16:24 +0700

    change syntax highlighter to tsx

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -2,6 +2,9 @@
 export { default as Heading } from './article/Heading';
 export { default as SyntaxHighlighter } from './article/SyntaxHighlighter';
 
+// Code
+export { default as SyntaxHighlighter } from './code/SyntaxHighlighter';
+
 // Icon
 export { default as Icon } from './icon/Icon';
 
