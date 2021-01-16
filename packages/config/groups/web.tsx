84a0d9ab12c6fac46ba75d6968b69bf3962e6f08
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2021-01-17 04:31:06 +0700

    Change intro

--- packages/config/groups/web.tsx
+++ packages/config/groups/web.tsx
@@ -1,8 +1,20 @@
+import React from 'react';
 import { TopicGroup } from '@pelicin/config';
 
 const group: TopicGroup = {
   groupTitle: 'Web',
-  groupDescription: '',
+  groupDescription: (
+    <p>
+      Various web development essentials curated from{' '}
+      <a href="https://developer.mozilla.org/en-US/docs/Learn">MDN</a> and{' '}
+      <cite>
+        <a href="https://frontendmasters.com/books/front-end-handbook/2019/#1.2">
+          Front End Developer Handbook 2019
+        </a>{' '}
+      </cite>
+      by Frontend Masters.
+    </p>
+  ),
   groupHash: 'web',
   sections: [
     {
