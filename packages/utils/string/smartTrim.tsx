1570b14c1fb2ea0f786a8a831a23dc45b8f6605e
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2021-02-13 03:50:27 +0700

    allow array on smart trim

--- packages/utils/string/smartTrim.tsx
+++ packages/utils/string/smartTrim.tsx
@@ -1,8 +1,22 @@
-export default function smartTrim(string) {
-  const prefixWhitespaceMatch = string.match(/^\n[ ]*/);
-  // '\n\s\s\s\s' -> 4, because there are 4 spaces
+/**
+ * Trims and attempts to reset indentation of string.
+ * (E.g. if every line is prefixed by a minimum of 2 leading spaces, we will remove those 2
+ * leading spaces from each line.)
+ *
+ * We allow array of string as an "escape-hatch" to put empty line on the string on MDX files,
+ * since if we put the empty line literally in the string literal, MDX will incorrectly parse it
+ * as paragraph break and throws error.
+ *
+ * @param arg - string or array of string. If array of string is given, expects that the string
+ *   is prefixed and suffixed by newline character.
+ */
+export default function smartTrim(arg: string | string[]) {
+  const processedString = Array.isArray(arg) ? arg.join('') : arg;
+
+  // `ambientSpaceIndentCount` computes '\n\s\s\s\s' -> 4, because there are 4 spaces
+  const prefixWhitespaceMatch = processedString.match(/^\n[ ]*/);
   const ambientSpaceIndentCount = prefixWhitespaceMatch ? prefixWhitespaceMatch[0].length - 1 : -1;
-  let temp = string;
+  let temp = processedString;
 
   if (ambientSpaceIndentCount > 0) {
     const regex = new RegExp(`\\n${' '.repeat(ambientSpaceIndentCount)}`, 'g');
