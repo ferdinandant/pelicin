a3ef67a659f31b8d69caae2aa5d735a040b573b8
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2021-04-10 19:03:41 +0700

    allow accents on hash

--- packages/layout/index-layout/utils/extractHashFromTitle.tsx
+++ packages/layout/index-layout/utils/extractHashFromTitle.tsx
@@ -1,7 +1,7 @@
 export default function extractHashFromTitle(title: string) {
   const result = title
     .toLowerCase()
-    .replace(/[^A-Za-z0-9]/g, '-')
+    .replace(/[ ]/g, '-')
     .replace(/-{2,}/g, '-')
     .replace(/^-|-$/g, '');
   return result;
