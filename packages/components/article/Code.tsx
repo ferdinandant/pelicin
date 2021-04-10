445949cbb850e10b252b3ee39da2e2939204a227
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2021-04-11 02:56:52 +0700

    Write offset

--- packages/components/article/Code.tsx
+++ packages/components/article/Code.tsx
@@ -2,8 +2,16 @@ import React, { ReactChild } from 'react';
 
 type Props = {
   str: ReactChild;
+  var?: boolean;
 };
 
 export default function Code(props: Props) {
+  if (props.var) {
+    return (
+      <var>
+        <code>{props.str}</code>
+      </var>
+    );
+  }
   return <code>{props.str}</code>;
 }
