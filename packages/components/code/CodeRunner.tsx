214025f0c6e80beeb763afeb501996c6f94b7ee1
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2021-10-25 04:09:25 +0700

    Add stringify function

--- packages/components/code/CodeRunner.tsx
+++ packages/components/code/CodeRunner.tsx
@@ -1,7 +1,8 @@
-import React, { Fragment, useState, useCallback } from 'react';
+import React, { Fragment, useState, useCallback, useMemo } from 'react';
 import classNames from 'classnames';
 
-import { SyntaxEditor, Icon } from '@pelicin/components';
+import { SyntaxEditor } from '@pelicin/components';
+import { stringify } from '@pelicin/utils';
 
 // ================================================================================
 // TYPES/CONST
@@ -50,11 +51,70 @@ export default function CodeRunner({ language, code: initialCode }: Props) {
       const startTs = Date.now();
       fn(...argValues);
       const endTs = Date.now();
-      appendConsoleMessage([`Run succesfully in ${endTs - startTs} ms.`], 'gray');
+      appendConsoleMessage([`Run successfully in ${endTs - startTs} ms.`], 'gray');
     } catch (err) {
       appendConsoleMessage([err], 'red');
     }
   }, [consoleMessages, code]);
+
+  const consoleContent = useMemo(() => {
+    return (
+      <>
+        <div className="console">
+          {consoleMessages.map((msgLine, lineIdx) => (
+            <div key={lineIdx} className={classNames('line', msgLine.color)}>
+              {msgLine.lineBlocks.map((block, blockIdx, arr) => {
+                const string = stringify(block);
+                const hasNextItem = blockIdx < arr.length - 1;
+                return (
+                  <Fragment key={blockIdx}>
+                    <span>{string}</span>
+                    {hasNextItem && <span> </span>}
+                  </Fragment>
+                );
+              })}
+            </div>
+          ))}
+        </div>
+        <style jsx>{`
+          .console {
+            height: 150px;
+            outline: none;
+            font-family: source-code-pro, 'Menlo', 'Monaco', 'Consolas', 'Courier New', monospace;
+            font-size: var(--font-size-tiny);
+            color: var(--color-gray-1);
+            line-height: 1.5;
+            resize: vertical;
+            overflow: auto;
+          }
+          .console .line span {
+            white-space: pre;
+          }
+          .console .white {
+            color: var(--color-gray-1);
+          }
+          .console .red {
+            color: var(--color-red-3);
+          }
+          .console .yellow {
+            color: var(--color-yellow-3);
+          }
+          .console .green {
+            color: var(--color-green-3);
+          }
+          .console .blue {
+            color: var(--color-blue-3);
+          }
+          .console .purple {
+            color: var(--color-purple-3);
+          }
+          .console .gray {
+            color: var(--color-gray-4);
+          }
+        `}</style>
+      </>
+    );
+  }, [consoleMessages]);
 
   return (
     <>
@@ -67,26 +127,7 @@ export default function CodeRunner({ language, code: initialCode }: Props) {
           onReset={() => setCode(initialCode)}
         />
       </div>
-      {showConsole && (
-        <div className="consoleContainer">
-          <div className="console">
-            {consoleMessages.map((msgLine, lineIdx) => (
-              <div key={lineIdx} className={classNames('line', msgLine.color)}>
-                {msgLine.lineBlocks.map((block, blockIdx, arr) => {
-                  const string = toString(block);
-                  const hasNextItem = blockIdx < arr.length - 1;
-                  return (
-                    <Fragment key={blockIdx}>
-                      <span>{string}</span>
-                      {hasNextItem && <span> </span>}
-                    </Fragment>
-                  );
-                })}
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
+      {showConsole && <div className="consoleContainer">{consoleContent}</div>}
 
       <style jsx>{`
         .editor {
@@ -99,45 +140,9 @@ export default function CodeRunner({ language, code: initialCode }: Props) {
           background-color: var(--color-gray-7);
           border-bottom-left-radius: var(--border-radius-normal);
           border-bottom-right-radius: var(--border-radius-normal);
+          box-shadow: 0 0 90px var(--color-gray-9) inset;
           padding: 12px;
           z-index: 1;
-        }
-
-        .console {
-          height: 120px;
-          outline: none;
-          font-family: source-code-pro, 'Menlo', 'Monaco', 'Consolas', 'Courier New', monospace;
-          background-color: var(--color-gray-7);
-          font-size: var(--font-size-tiny);
-          color: var(--color-gray-1);
-          line-height: 1.5;
-          resize: vertical;
-          overflow: auto;
-        }
-        .console .line span {
-          white-space: pre;
-        }
-
-        .console .white {
-          color: var(--color-gray-1);
-        }
-        .console .red {
-          color: var(--color-red-3);
-        }
-        .console .yellow {
-          color: var(--color-yellow-3);
-        }
-        .console .green {
-          color: var(--color-green-3);
-        }
-        .console .blue {
-          color: var(--color-blue-3);
-        }
-        .console .purple {
-          color: var(--color-purple-3);
-        }
-        .console .gray {
-          color: var(--color-gray-4);
         }
       `}</style>
     </>
@@ -162,34 +167,4 @@ function getConsoleSubstitute(
     debug: (...blocks) => appendConsoleMessage(blocks, 'blue'),
     trace: (...blocks) => appendConsoleMessage(blocks, 'purple'),
   };
-}
-
-function toString(obj: any): string {
-  if (obj === null || typeof obj !== 'object') {
-    return String(obj);
-  }
-  if (obj instanceof Error) {
-    return stringifyError(obj);
-  }
-  return JSON.stringify(obj, null, 2);
-}
-
-function stringifyError(obj: Error): string {
-  const { message, stack } = obj;
-  const stackLines = stack.split('\n');
-  // Only consider the top lines that end with 'eval:<number>:<number>'
-  // Lines would be like: 'printHello@<file> line 11 > eval line 89 > Function line 3 > eval:4:9'
-  const reducedPrettiedLines = [];
-  const evalSuffixRegex = /(.*?)@.*(eval:\d+:\d+)$/;
-  for (const line of stackLines) {
-    const match = line.match(evalSuffixRegex);
-    if (match) {
-      const [_, functionName, location] = match;
-      const prettiedLine = `  at ${functionName || '<anonymous>'} (${location})`;
-      reducedPrettiedLines.push(prettiedLine);
-    } else {
-      break;
-    }
-  }
-  return `Error: ${message}\n${reducedPrettiedLines.join('\n')}`;
 }
