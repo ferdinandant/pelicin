595787136554689958289dcab9e5f374aa24aa16
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2021-11-24 06:06:54 +0700

    Use next mdx support

--- next.config.js
+++ next.config.js
@@ -1,22 +1,14 @@
-// Enabling MDX support
-const mdxRule = ({ defaultLoaders }) => ({
-  test: /\.mdx/,
-  use: [
-    defaultLoaders.babel,
-    {
-      loader: '@mdx-js/loader',
-      options: {},
-    },
-  ],
+/* eslint @typescript-eslint/no-var-requires: 0 */
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
 });
 
-module.exports = {
-  webpack: (config, { defaultLoaders }) => {
-    config.module.rules.push(mdxRule({ defaultLoaders }));
+module.exports = withMDX({
+  webpack: (config) => {
     return config;
   },
   publicRuntimeConfig: {
     showSample: Boolean(process.env.NODE_ENV !== 'production'),
   },
   pageExtensions: ['mdx', 'tsx'],
-};
+});
