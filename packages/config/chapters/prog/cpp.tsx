2ff72ec20a577a03e7c72f41a00b81b70426cc6e
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2022-01-23 14:07:51 +0700

    Add intro and attribution

---
+++ packages/config/chapters/prog/cpp.tsx
@@ -0,0 +1,24 @@
+import { TopicChapters } from '@pelicin/config';
+
+const chapters: TopicChapters = [
+  {
+    chapter: null,
+    children: [
+      {
+        title: 'Introduction',
+        path: '/prog/cpp',
+      },
+      {
+        title: 'Attribution',
+        path: '/prog/cpp/attribution',
+      },
+      {
+        title: 'Setup',
+        path: '/prog/cpp/setup',
+        badge: 'TODO'
+      },
+    ],
+  },
+]
+
+export default chapters;
