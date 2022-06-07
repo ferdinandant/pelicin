0151c141886517a6c9afbcf5e486b2a6229cae00
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2022-06-07 08:36:23 +0700

    Implement component map

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
+
+// https://nextjs.org/docs/advanced-features/using-mdx#setup-nextmdx-in-nextjs
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    providerImportSource: '@mdx-js/react',
+  },
 });
 
 module.exports = withMDX({
--- packages/layout/article-layout/MDXArticleLayout.tsx
+++ packages/layout/article-layout/MDXArticleLayout.tsx
@@ -1,11 +1,14 @@
 import React, { ReactNode } from 'react';
+import { MDXProvider } from '@mdx-js/react';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 import {
   getTopicConfigFromPath,
   TopicConfigProvider,
   TopicChaptersProvider,
+  useTopicConfig,
 } from '@pelicin/config';
 import {
   TopBar,
@@ -15,12 +18,14 @@ import {
   TOCToggleProvider,
   MainContainer,
   getArticleTOC,
-  processArticleMDX,
+  extractHashFromNode,
 } from '@pelicin/layout';
+import { Heading, SyntaxHighlighter } from '@pelicin/components';
+import { extractStringFromNode } from '@pelicin/utils';
 
 const HamburgerChapterSidebar = dynamic(() => import('./components/HamburgerChapterSidebar'), {
   ssr: false,
-});
+}) as any;
 
 // ================================================================================
 // TYPES/CONST
@@ -30,6 +35,16 @@ type Props = {
   children: ReactNode;
 };
 
+const componentMap = {
+  h1: (props) => <TitleHeader {...props} />,
+  h2: (props) => <ContentHeader heading="h2" {...props} />,
+  h3: (props) => <ContentHeader heading="h3" {...props} />,
+  h4: (props) => <ContentHeader heading="h4" {...props} />,
+  h5: (props) => <ContentHeader heading="h5" {...props} />,
+  h6: (props) => <ContentHeader heading="h6" {...props} />,
+  pre: (props) => <PreformattedBlock {...props} />,
+};
+
 // ================================================================================
 // MAIN
 // ================================================================================
@@ -37,11 +52,15 @@ type Props = {
 export default function MDXArticleLayout(props: Props) {
   const { children } = props;
   const { pathname } = useRouter();
+
+  // Topic
   const topicConfig = getTopicConfigFromPath(pathname);
   const { topicKey } = topicConfig;
 
+  // TOC
+  // Currently hot reloading does not work after we upgrade to @mdx-js v2
+  // Might want to investigate later; but current;y it's not blocking
   const toc = getArticleTOC(children);
-  const processedChildren = processArticleMDX(children);
 
   return (
     <TopicConfigProvider value={topicConfig}>
@@ -53,7 +72,9 @@ export default function MDXArticleLayout(props: Props) {
               <div>
                 <ChapterSidebar />
                 <HamburgerChapterSidebar />
-                <MainContainer>{processedChildren}</MainContainer>
+                <MainContainer>
+                  <MDXProvider components={componentMap}>{children as any}</MDXProvider>
+                </MainContainer>
               </div>
             </ArticleTOCProvider>
             <style jsx>{`
@@ -66,4 +87,49 @@ export default function MDXArticleLayout(props: Props) {
       </TopicChaptersProvider>
     </TopicConfigProvider>
   );
+}
+
+// ================================================================================
+// CHILDREN
+// ================================================================================
+
+function TitleHeader(props) {
+  const { children } = props;
+  const { topicTitle } = useTopicConfig();
+  const anchorHash = extractHashFromNode(children);
+  const titleString = extractStringFromNode(children);
+  const siteTopicString = topicTitle ? `Pelicin / ${topicTitle}` : 'Pelicin';
+  return (
+    <React.Fragment>
+      <Head key="title">
+        <title>
+          {titleString} - {siteTopicString}
+        </title>
+      </Head>
+      <Heading heading="h1" anchor={anchorHash}>
+        {children}
+      </Heading>
+    </React.Fragment>
+  );
+}
+
+function ContentHeader(props) {
+  const { children, heading } = props;
+  const anchorHash = extractHashFromNode(children);
+  return (
+    <Heading heading={heading} anchor={anchorHash}>
+      {children}
+    </Heading>
+  );
+}
+
+function PreformattedBlock(props) {
+  const { children } = props;
+  const { props: childrenProps } = children as any;
+  const { className, children: codeString } = childrenProps || {};
+  if (className && className.startsWith('language-')) {
+    const language = className.replace('language-', '');
+    return <SyntaxHighlighter language={language}>{codeString}</SyntaxHighlighter>;
+  }
+  return children;
 }
