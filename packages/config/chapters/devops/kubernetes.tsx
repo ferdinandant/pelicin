e24a3171d2e9f77d5c79304d3e0402fc16c8f2a7
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2022-07-11 07:02:43 +0700

    Add kubernetes stub

---
+++ packages/config/chapters/devops/kubernetes.tsx
@@ -0,0 +1,19 @@
+import { TopicChapters } from '@pelicin/config';
+
+const chapters: TopicChapters = [
+  {
+    chapter: null,
+    children: [
+      {
+        title: 'Introduction',
+        path: '/devops/kubernetes',
+      },
+      {
+        title: 'References',
+        path: '/devops/kubernetes/references',
+      },
+    ],
+  },
+]
+
+export default chapters;
---
+++ packages/config/groups/devops.tsx
@@ -0,0 +1,18 @@
+import { TopicGroup } from '@pelicin/config';
+
+const group: TopicGroup = {
+  groupTitle: 'DevOps',
+  groupHash: 'devops',
+  sections: [
+    {
+      topics: [
+        {
+          title: 'Kubernetes',
+          path: '/devops/kubernetes',
+        },
+      ],
+    },
+  ],
+};
+
+export default group;
